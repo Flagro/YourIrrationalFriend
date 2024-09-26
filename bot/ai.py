baee3a9b0442f9dc15bdaa958402086e3154de7e
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-09-26 18:30:22 +0200

    Pass the default_text_model_name as a parameter

--- bot/ai.py
+++ bot/ai.py
@@ -4,47 +4,14 @@ from openai import OpenAI
 
 
 class AI:
-    def __init__(self, openai_api_key: str):
+    def __init__(self, openai_api_key: str, default_text_model_name: str):
         self.llm = OpenAI(
             api_key=openai_api_key, model=self._get_default_text_model_name()
         )
         self.vision_model = OpenAI(
             api_key=openai_api_key,
-            model=self._get_default_vision_model_name(),
+            model=default_text_model_name,
         )
-        # TODO: fix this - this is not OpenAI object
-        self.image_generation_model = OpenAI(
-            api_key=openai_api_key,
-            model=self._get_default_image_generation_model_name(),
-        )
-
-    def _get_default_model_name(
-        self, model_type: Literal["text", "vision", "image_generation"]
-    ) -> str:
-        params_dict = {
-            "text": {
-                "models_dict": self.ai_config.TextGeneration.Models,
-                "default_attr": "text_default",
-            },
-            "vision": {
-                "models_dict": self.ai_config.TextGeneration.Models,
-                "default_attr": "vision_default",
-            },
-            "image_generation": {
-                "models_dict": self.ai_config.ImageGeneration.Models,
-                "default_attr": "image_generation_default",
-            },
-        }
-        first_model = None
-        for model in params_dict[model_type]["models_dict"].values():
-            if getattr(model, params_dict[model_type]["default_attr"]):
-                return model.name
-            if first_model is None:
-                first_model = model.name
-        return first_model
-
-    def _get_default_text_model_name(self) -> str:
-        return self._get_default_model_name("text")
 
     async def is_content_acceptable(self, text: str):
         # TODO: implement this
